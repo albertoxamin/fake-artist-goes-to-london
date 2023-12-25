d096a3d3fcbf87f687f4db05b82fc474ef37b685
Alberto Xamin <alberto.xamin@taktile.com>
2023-12-25 19:00:12 +0100

    initial game logic

--- index.js
+++ index.js
@@ -1,45 +1,70 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const { gameManager } = require('./managers');
 
 const app = express();
+app.use(express.static('public'));
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const topics = ['Animals', 'Places', 'Objects', 'Food']; // Example topics
+let playerRoomMap = {};
 
-const {gameManager} = require('./managers');
 
 io.on('connection', (socket) => {
-    socket.on('createRoom', (roomCode) => {
-        gameManager.createRoom(roomCode);
-        socket.join(roomCode);
-        // Handle room creation acknowledgment...
-    });
+	socket.on('createRoom', (roomCode) => {
+		if (gameManager.getRoom(roomCode)) {
+			socket.emit('error', 'Room already exists');
+			return;
+		}
+		const room = gameManager.createRoom(roomCode);
+		socket.join(roomCode);
+		socket.emit('roomCreated', roomCode);
+		room.addPlayer(socket.id);
+		playerRoomMap[socket.id] = roomCode; // Store player's room
+		io.to(roomCode).emit('playerCount', room.players.length);
+	});
 
-    socket.on('joinRoom', (roomCode) => {
-        const room = gameManager.getRoom(roomCode);
-        if (room) {
-            room.addPlayer(socket.id);
-            socket.join(roomCode);
-            // Handle player joined acknowledgment...
-        } else {
-            // Handle error...
-        }
-    });
+	socket.on('joinRoom', (roomCode) => {
+		const room = gameManager.getRoom(roomCode);
+		if (room) {
+			room.addPlayer(socket.id);
+			socket.join(roomCode);
+			socket.emit('joinedRoom', roomCode);
+			io.to(roomCode).emit('playerCount', room.players.length);
+			playerRoomMap[socket.id] = roomCode; // Store player's room
+		} else {
+			socket.emit('error', 'Room does not exist');
+		}
+	});
 
-    socket.on('startGame', (roomCode) => {
-        try {
-            const gameInfo = gameManager.startGameInRoom(roomCode);
-            // Notify players with their roles and topic (if not fake artist)...
-        } catch (error) {
-            // Handle error...
-        }
-    });
+	socket.on('startGame', (roomCode) => {
+		try {
+			const room = gameManager.getRoom(roomCode);
+			const gameInfo = room.startGame(gameManager.topics);
+			io.to(roomCode).emit('gameStarted', { topic: gameInfo.topic });
+			room.players.forEach(playerId => {
+				const role = room.playersRoles[playerId];
+				io.to(playerId).emit('role', { role: role, topic: role === 'Artist' ? gameInfo.topic : undefined });
+			});
+		} catch (error) {
+			socket.emit('error', error.message);
+		}
+	});
 
-    // Additional event handlers...
+	socket.on('disconnect', () => {
+		const roomCode = playerRoomMap[socket.id];
+		if (roomCode) {
+			const room = gameManager.getRoom(roomCode);
+			if (room) {
+				room.removePlayer(socket.id); // You might need to implement this method
+				delete playerRoomMap[socket.id]; // Remove the player from the map
+				io.to(roomCode).emit('playerCount', room.players.length);
+			}
+		}
+	});
 });
 
 server.listen(3000, () => {
-	console.log('Listening on *:3000');
+	console.log('Server listening on port 3000');
 });
---
+++ public/script.js
@@ -0,0 +1,69 @@
+const socket = io();
+
+const roomCodeInput = document.getElementById('roomCode');
+const createRoomButton = document.getElementById('createRoom');
+const joinRoomButton = document.getElementById('joinRoom');
+const startGameButton = document.getElementById('startGame');
+const gameStatusDiv = document.getElementById('gameStatus');
+const playerRoleDiv = document.getElementById('playerRole');
+
+createRoomButton.addEventListener('click', () => {
+	const roomCode = roomCodeInput.value;
+	socket.emit('createRoom', roomCode);
+});
+
+joinRoomButton.addEventListener('click', () => {
+	const roomCode = roomCodeInput.value;
+	socket.emit('joinRoom', roomCode);
+});
+
+startGameButton.addEventListener('click', () => {
+	const roomCode = roomCodeInput.value;
+	socket.emit('startGame', roomCode);
+});
+
+function hideRoomActions() {
+	createRoomButton.style.display = 'none';
+	joinRoomButton.style.display = 'none';
+	roomCodeInput.style.display = 'none';
+}
+
+socket.on('roomCreated', (roomCode) => {
+	gameStatusDiv.innerText = `Room created: ${roomCode}`;
+	hideRoomActions();
+});
+
+socket.on('joinedRoom', (roomCode) => {
+	gameStatusDiv.innerText = `Joined room: ${roomCode}`;
+	hideRoomActions();
+});
+
+socket.on('gameStarted', (data) => {
+	gameStatusDiv.innerText = `Game started. Topic: ${data.topic}`;
+});
+
+const roleCardDiv = document.getElementById('roleCard');
+const playerRoleP = document.getElementById('playerRole');
+const gameTopicP = document.getElementById('gameTopic');
+
+socket.on('role', (data) => {
+	playerRoleP.innerText = `Your role: ${data.role}`;
+	if (data.topic) {
+		gameTopicP.innerText = `Topic: ${data.topic}`;
+	} else {
+		gameTopicP.innerText = '';
+	}
+	roleCardDiv.style.display = 'block'; // Show the card
+});
+
+const playerCountDiv = document.getElementById('playerCount');
+
+
+socket.on('playerCount', (count) => {
+	playerCountDiv.innerText = `Players in room: ${count}`;
+});
+
+
+socket.on('error', (message) => {
+	alert(message);
+});
