ba5efe404c8e9eba2bf433fc7a7f547cc702c987
Alberto Xamin <alberto.xamin@taktile.com>
2023-12-25 19:04:50 +0100

    add removeplayer

--- managers.js
+++ managers.js
@@ -1,58 +1,105 @@
 class Room {
-    constructor(code) {
-        this.code = code;
-        this.players = [];
-        this.gameStarted = false;
-        this.playersRoles = {};
-        this.topic = null;
-    }
+	constructor(code) {
+		this.code = code;
+		this.players = [];
+		this.gameStarted = false;
+		this.playersRoles = {};
+		this.topic = null;
+	}
 
-    addPlayer(playerId) {
-        this.players.push(playerId);
-    }
+	addPlayer(playerId) {
+		this.players.push(playerId);
+	}
 
-    startGame(topics) {
-        if (this.players.length < 3) {
-            throw new Error('Not enough players');
-        }
+	startGame(topics) {
+		if (this.players.length < 3) {
+			throw new Error('Not enough players');
+		}
 
-        this.gameStarted = true;
-        const fakeArtistIndex = Math.floor(Math.random() * this.players.length);
-        this.topic = topics[Math.floor(Math.random() * topics.length)];
+		this.gameStarted = true;
+		const fakeArtistIndex = Math.floor(Math.random() * this.players.length);
+		this.topic = topics[Math.floor(Math.random() * topics.length)];
 
-        this.players.forEach((playerId, index) => {
-            this.playersRoles[playerId] = index === fakeArtistIndex ? 'Fake Artist' : 'Artist';
-        });
+		this.players.forEach((playerId, index) => {
+			this.playersRoles[playerId] = index === fakeArtistIndex ? 'Fake Artist' : 'Artist';
+		});
 
-        return { topic: this.topic, playersRoles: this.playersRoles };
-    }
+		return { topic: this.topic, playersRoles: this.playersRoles };
+	}
 
-    // Additional methods as needed...
+	removePlayer(playerId) {
+		this.players = this.players.filter(player => player !== playerId);
+	}
 }
 
 const gameManager = {
-    rooms: {},
-    topics: ['Animals', 'Places', 'Objects', 'Food'],
+	rooms: {},
+	topics: [
+		'Apple',
+		'Car',
+		'Cat',
+		'Chair',
+		'Chicken',
+		'Clock',
+		'Dog',
+		'Egg',
+		'Elephant',
+		'Fire',
+		'Fish',
+		'Flower',
+		'Frog',
+		'Grass',
+		'House',
+		'Leaf',
+		'Milk',
+		'Money',
+		'Monkey',
+		'Moon',
+		'Motorcycle',
+		'Mountain',
+		'Pants',
+		'Pencil',
+		'Pizza',
+		'Plant',
+		'Potato',
+		'Rain',
+		'Road',
+		'Rock',
+		'Sandwich',
+		'School',
+		'Ship',
+		'Snake',
+		'Snow',
+		'Socks',
+		'Stairs',
+		'Streetlight',
+		'Sun',
+		'Sword',
+		'Table',
+		'Tree',
+		'Water',
+		'Window',
+	],
 
-    createRoom(code) {
-        const newRoom = new Room(code);
-        this.rooms[code] = newRoom;
-        return newRoom;
-    },
+	createRoom(code) {
+		const newRoom = new Room(code);
+		this.rooms[code] = newRoom;
+		return newRoom;
+	},
 
-    getRoom(code) {
-        return this.rooms[code];
-    },
+	getRoom(code) {
+		return this.rooms[code];
+	},
 
-    startGameInRoom(code) {
-        const room = this.getRoom(code);
-        if (!room) {
-            throw new Error('Room does not exist');
-        }
-        return room.startGame(this.topics);
-    },
+	startGameInRoom(code) {
+		const room = this.getRoom(code);
+		if (!room) {
+			throw new Error('Room does not exist');
+		}
+		return room.startGame(this.topics);
+	},
 
-    // Additional methods as needed...
+	// Additional methods as needed...
 };
 
 module.exports = { Room, gameManager };
